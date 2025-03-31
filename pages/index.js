615ba314d443813748175e3279e6a73811cff2b0
KIM-SEUNG-JIN <34882148+KIM-SEUNG-JIN@users.noreply.github.com>
2025-03-31 16:22:54 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#f67280' }}>모바일 청첩장</h1>
+      <p>신랑 ♥ 신부</p>
+      <p>2023년 12월 31일 오후 12시</p>
+      <p>웨딩홀</p>
+      <div style={{ marginTop: '2rem' }}>
+        <p>곧 더 많은 내용이 추가될 예정입니다.</p>
+      </div>
+    </div>
+  )
+}
